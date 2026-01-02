a964676970749afb5dc8ad5ef591864bfeeeac19
M <m@mail.com>
2026-01-02 12:37:21 +0100

    Created const in javascript

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -1,2 +1,8 @@
-console.log('Hello World!');
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const message = document.getElementById("message");
+const subject = document.getElementById("subject");
+const submit = document.getElementById("submit");
 
